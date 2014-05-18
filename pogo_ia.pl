a3530e289274b038195c18f6582e25c3dee4daf9
belkacem-tosh-win8 <belkacem.lahouel@gmail.com>
2014-05-18 14:42:14 +0200

    Updating rules and prototype...

--- pogo_ia.pl
+++ pogo_ia.pl
@@ -4,7 +4,7 @@
 %           =>            with * white pawns and - black pawns
 % -- -- --      -- -- --
 
-think(1, 1, 2, [[0, 0], [0, 0], [0, 0], [], [], [], [1, 1], [1, 1], [1, 1]], 0).
+% think(1, 1, 2, [[0, 0], [0, 0], [0, 0], [], [], [], [1, 1], [1, 1], [1, 1]], 0).
 
 % think(nCaseStart, idxPawn, nCaseEnd, Board, colorPlayer)
 % We should add another predicate with the current evaluation
@@ -16,4 +16,19 @@ think(1, 1, 2, [[0, 0], [0, 0], [0, 0], [], [], [], [1, 1], [1, 1], [1, 1]], 0).
 % idxPawn : [0 1 2 3 ...]
 
 % Colors : White = 1, Black = 0
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% White: 1, Black: 0
+% Board is coded as following: -1 means "go to next case"
+%				Then we have pawns, from top to bottom
+%				and again -1 to move forward
+% Normally for the answer: we return the position of the cases/pawns
+%			   Beginning with 1 -> n
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+think(1, 1, 2, [1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1], 0).
+% Initial situation description. We move the first pawn in stack #1 to #2.
+
+
+
 
