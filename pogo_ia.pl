fff8ab8f0a8255f09f7eb3fe03267b7b0ca3d78f
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-05-24 21:31:19 +0200

    Documentation du prédicat "think", qui sert d'interface

--- pogo_ia.pl
+++ pogo_ia.pl
@@ -1,34 +1,42 @@
-% Testing AIPlayer with one fact
+% #############################################################################
+% think([ETAT], [cJoueur, lvlAI], [nCaseDepart, nCaseArrivee, indexPionStack]).
+% #############################################################################
 
-% ** ** **      * *** **
-%           =>            with * white pawns and - black pawns
-% -- -- --      -- -- --
+% -----------------------------------------------------------------------------
+% ETAT est codé comme suit :
+%		-1 pour dire qu'on passe à la case suivante
+%		1  pour désigner un pion blanc
+%		0  pour désigner un pion noir
+% De cette manière, on a toujours un tableau de 21 charactères.
+%
+% Chaque tas de pions est considéré comme étant un stack
+%		Le premier est celui du haut
+% 		Le dernier est celui du bas
+% Cela devrait nous arranger lors de la réflexion Prolog.
+% -----------------------------------------------------------------------------
 
-% think(1, 1, 2, [[0, 0], [0, 0], [0, 0], [], [], [], [1, 1], [1, 1], [1, 1]], 0).
+% -----------------------------------------------------------------------------
+% cJoueur est la couleur joueur
+%		1 pour le joueur blanc
+%		0 pour le joueur noir
+% -----------------------------------------------------------------------------
 
-% think(nCaseStart, idxPawn, nCaseEnd, Board, colorPlayer)
-% We should add another predicate with the current evaluation
+% -----------------------------------------------------------------------------
+% lvlAI est le niveau de l'intelligence artificelle
+%		C'est normalement un nombre dans {0, 1, 2, 3}
+%		(à vérifier) où 0 => arboresence de recherche vide : hasard ?
+% -----------------------------------------------------------------------------
 
+% -----------------------------------------------------------------------------
+% nCaseDepart     numéro de la case de départ
+% nCaseArrivee    numéro de la case d'arrivée, du pion qu'on jouera
+% indexPionStack  index du pion "à la mode stack"
+% -----------------------------------------------------------------------------
+
+% -----------------------------------------------------------------------------
+% Les cases sont numérotées comme suit :
 % 1 2 3
 % 4 5 6
-% 7 8 9 : Board configuration here
-
-% idxPawn : [0 1 2 3 ...]
-
-% Colors : White = 1, Black = 0
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% White: 1, Black: 0
-% Board is coded as following: -1 means "go to next case"
-%				Then we have pawns, from top to bottom
-%				and again -1 to move forward
-% Normally for the answer: we return the position of the cases/pawns
-%			   Beginning with 1 -> n
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-think(1, 1, 2, [1, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1], 0).
-% Initial situation description. We move the first pawn in stack #1 to #2.
-
-
-
+% 7 8 9
+% -----------------------------------------------------------------------------
 
