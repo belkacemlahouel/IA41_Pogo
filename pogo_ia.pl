f9211dd3f5614471b84e1afa86e0d522425a68d7
belkacem-tosh-win8 <belkacem.lahouel@gmail.com>
2014-06-12 21:56:23 +0200

    Erreur orthographique dérangeante sur l'interface graphique

--- pogo_ia.pl
+++ pogo_ia.pl
@@ -64,8 +64,8 @@ eval_pion(0, -1).		% pion noir
 
 % --- essais
 eval(ETAT, E, LEVEL) :- 
-	(LEVEL = 0,eval5(ETAT, E),!;
-	 LEVEL = 1,eval2(ETAT, E),!;
+	(LEVEL = 0,eval0(ETAT, E),!; % Blancs
+	 LEVEL = 1,eval5(ETAT, E),!; % Noirs
 	 LEVEL = 2,eval2(ETAT, E)). % ETAT, EVAL, COMPTEUR (4 premiers pions)
 % ------------------------
 % ------------------------
